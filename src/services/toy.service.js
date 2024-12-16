4f7bf88cbf0d2d2eae692de6066747a1fc02decd
DrorGaon <drordtuii@gmail.com>
2024-12-16 23:42:47 +0200

    Fix next and prev scrolling in toy details

--- src/services/toy.service.js
+++ src/services/toy.service.js
@@ -4,7 +4,6 @@ import { httpService } from './http.service.js'
 
 const TOY_KEY = 'toy'
 const BASE_URL = 'toy/'
-_createToys()
 
 export const toyService = {
     query,
@@ -22,6 +21,7 @@ function query(filterBy = {}) {
 
 function get(toyId) {
     return httpService.get(BASE_URL + toyId)
+        .then(toy => _setNextPrevToyId(toy))
 }
 
 function remove(toyId) {
@@ -53,27 +53,8 @@ function getFilterFromSearchParams(searchParams) {
     return filterBy
 }
 
-function _createToys() {
-    let toys = utilService.loadFromStorage(TOY_KEY)
-    if (!toys || !toys.length) {
-        toys = []
-        const names = ['Toy car', 'Dog plushie', 'Soccer ball', 'Jigsaw puzzle', 'Lego set']
-        for (let i = 0; i < 10; i++) {
-            const name = names[utilService.getRandomIntInclusive(0, names.length - 1)]
-            toys.push(_createToy(name, utilService.getRandomIntInclusive(30, 100)))
-        }
-        utilService.saveToStorage(TOY_KEY, toys)
-    }
-}
-
-function _createToy(name, price) {
-    const toy = getEmptyToy(name, price)
-    toy._id = utilService.makeId()
-    return toy
-}
-
 function _setNextPrevToyId(toy) {
-    return storageService.query(TOY_KEY).then((toys) => {
+    return httpService.get(BASE_URL).then((toys) => {
         const toyIdx = toys.findIndex((currToy) => currToy._id === toy._id)
         const nextToy = toys[toyIdx + 1] ? toys[toyIdx + 1] : toys[0]
         const prevToy = toys[toyIdx - 1] ? toys[toyIdx - 1] : toys[toys.length - 1]
